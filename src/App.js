a3cb10ef6b81dbda55e7a40dd25bcb2401b90210
Amit kumar <91358392+amitkumarmeena@users.noreply.github.com>
2022-11-29 18:44:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/current-weather/current-weather";
+import Forecast from "./components/forecast/forecast";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "./api";
+import "./App.css";
+
+function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
+  const handleOnSearchChange = (searchData) => {
+    const [city] = searchData.value;
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?q=${city}&appid=${WEATHER_API_KEY}`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?q=${city}&appid=${WEATHER_API_KEY}`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const weatherResponse = await response[0].json();
+        const forcastResponse = await response[1].json();
+
+        setCurrentWeather({ city: searchData.label, ...weatherResponse });
+        setForecast({ city: searchData.label, ...forcastResponse });
+      })
+      .catch(console.log);
+  };
+
+  return (
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
+      {forecast && <Forecast data={forecast} />}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  // headers: {
+  //   "X-RapidAPI-Key": "",// enter your rapid api key here
+  //   "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  // },
+};
+export const GEO_API_URL = "https://countriesnow.space/api/v0.1/countries";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "794ee95e63c5a32aaf88cd813fa2e425";
+
